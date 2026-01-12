63c27ec49e194aa715d9ceea90421ee2bbef5ee4
Ygor Nascimento <ygor.nascimento@outlook.com>
2026-01-12 09:42:06 -0300

    Adding vs folder to gitignore

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpLogging(opts =>
+    opts.LoggingFields = HttpLoggingFields.RequestProperties);
+
+builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpLogging();
+}
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/person", () => new Person("Ygor", "Nascimento"));
+
+app.Run();
+
+public record Person(string FirstName, string LastName);
